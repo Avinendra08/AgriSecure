ccc2df82ebd99fcfadc4a97f41dc19d7ee49350a
Saksham Agarwal <sakshamagarwal1616@gmail.com>
2024-09-07 03:26:05 +0530

    bid crud operations

---
+++ src/routes/farmerBid.route.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createFarmerBid,
+  getFarmerBids,
+  getFarmerBidById,
+  updateFarmerBid,
+  deleteFarmerBid,
+} from '../controllers/farmerBid.controller.js';
+
+const router = express.Router();
+
+router.post('/', createFarmerBid);
+router.get('/', getFarmerBids); //if kuch extra chahiya toh
+router.get('/:farmer_id', getFarmerBidById); //read with id krna hai toh
+router.put('/:farmer_id', updateFarmerBid);
+router.delete('/:farmer_id', deleteFarmerBid);
+
+export default router;
